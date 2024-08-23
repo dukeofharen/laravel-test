a0f7aba81205b8534c0d1c86cb62177e0d0a8d65
Duco Winterwerp <duco@wiersma-ict.nl>
2024-08-23 15:08:32 +0200

    Added endpoints for retrieving facts

--- app/Http/Controllers/FactController.php
+++ app/Http/Controllers/FactController.php
@@ -5,45 +5,46 @@ namespace App\Http\Controllers;
 use App\Models\Fact;
 use Illuminate\Http\Request;
 
-class FactController extends Controller
-{
+class FactController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
+    public function index() {
+        return Fact::all();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
+    public function store( Request $request ) {
         //
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Fact $fact)
-    {
-        //
+    public function show( $id ) {
+        return Fact::findOrFail($id);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function random() {
+        return Fact::inRandomOrder()->first();
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Fact $fact)
-    {
+    public function update( Request $request, Fact $fact ) {
         //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Fact $fact)
-    {
+    public function destroy( Fact $fact ) {
         //
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\FactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get( '/user', function ( Request $request ) {
     return $request->user();
-})->middleware('auth:sanctum');
+} )->middleware( 'auth:sanctum' );
+
+Route::get( 'facts', [ FactController::class, 'index' ] );
+Route::get('facts/random', [ FactController::class, 'random' ] );
+Route::get('facts/{id}', [ FactController::class, 'show' ] );
